9c29b0b4f1c45a518174c07221b598b95ad3ae63
faco229 <faco229@uky.edu>
2025-03-06 15:03:21 -0500

    Revert "changes"
    This reverts commit 1c03ff16bc8cb2655ce273450b361b26dcc86857.

--- sketch_march6.ino
+++ sketch_march6.ino
@@ -40,7 +40,3 @@ void loop() {
 }
 
 //Comment
-// const int PWMB = 10; //speed control pin on the motor driver for the left motor
-//const int BIN2 = 9;  //control pin 2 on the motor driver for the left motor
-//const int BIN1 = 8;  //control pin 1 on the motor driver for the left motor
-
